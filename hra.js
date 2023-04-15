b947f7f25baeb795f54468ed1537691c74d1fd56
Eliška Buociková <buocikovaeliska@gmail.com>
2023-04-15 18:59:57 +0200

    vybrání všech tlačítek v herní ploše a přidání posluchače události

--- hra.js
+++ hra.js
@@ -6,16 +6,7 @@ if (currentPlayer === 'circle') {
   playerIcon.src = 'circle.svg';
 }
 
-const button1 = document.querySelector('button:nth-child(1)');
-const button2 = document.querySelector('button:nth-child(2)');
-const button3 = document.querySelector('button:nth-child(3)');
-const button4 = document.querySelector('button:nth-child(4)');
-const button5 = document.querySelector('button:nth-child(5)');
-const button6 = document.querySelector('button:nth-child(6)');
-const button7 = document.querySelector('button:nth-child(7)');
-const button8 = document.querySelector('button:nth-child(8)');
-const button9 = document.querySelector('button:nth-child(9)');
-const button10 = document.querySelector('button:nth-child(10)');
+const buttons = document.querySelectorAll('button');
 
 const playedOut = (event) => {
   const move = event.target.classList;
@@ -32,16 +23,9 @@ const playedOut = (event) => {
   }
 };
 
-button1.addEventListener('click', playedOut);
-button2.addEventListener('click', playedOut);
-button3.addEventListener('click', playedOut);
-button4.addEventListener('click', playedOut);
-button5.addEventListener('click', playedOut);
-button6.addEventListener('click', playedOut);
-button7.addEventListener('click', playedOut);
-button8.addEventListener('click', playedOut);
-button9.addEventListener('click', playedOut);
-button10.addEventListener('click', playedOut);
+buttons.forEach((button) => {
+  button.addEventListener('click', playedOut);
+});
 
 const navigationFieldBlue = document.querySelector('.navigation__field--blue');
 
